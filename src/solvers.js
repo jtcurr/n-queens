36483a81354d200eabeb0aff3c7b8ca8409b581f
jtcurr <johntcurrie@gmail.com>
2016-12-21 20:59:10 -0800

    working on the solver

--- src/solvers.js
+++ src/solvers.js
@@ -12,18 +12,40 @@
 
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
-
-
-
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  //hardcode all combination of nRook board layout
+  //use our helper functions to pick the correct ones
+  //push one correct board to solution
+
+  //declare a new array 
+  // [[0, 1], [1, 0]]
+  // [[1, 0], [0, 1]]
+  var solution = [];
+  if (n === 1) {
+    solution = [[1]];
+  } 
+  if (n === 2) {
+    solution = [[0, 1], [1, 0]];
+  }
+  // var board = {
+  //   0: [0, 1], 
+  //   1: [1, 0]};
+  // };
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+  // check if solution pass our test
+  
   return solution;
+  
+  
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
+  //build a full decission tree
+  //use helper functions to eliminate nodes that don't pass nRooks test
+  //count the number of passed test
+
   var solutionCount = undefined; //fixme
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
